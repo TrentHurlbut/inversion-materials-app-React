7cc0b8e7a2fde8ca8c2a94b4715b15ea86e36bf1
TrentHurlbut <trent.hurlbut@gmail.com>
2022-01-05 09:23:12 -0800

    config prep for heroku deploy.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express'); //Line 1
 const app = express(); //Line 2
 const port = process.env.PORT || 3001; //Line 3
 const materials = require('./materials.json');
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
@@ -11,3 +14,7 @@ app.get('/express_backend', (req, res) => {
   //Line 9
   res.send(materials); //Line 10
 }); //Line 11
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
+});
