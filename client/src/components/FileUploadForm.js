af14543d3fd536f88f14c300c1c2e8cff522aea2
TrentHurlbut <trent.hurlbut@gmail.com>
2022-02-09 09:48:00 -0800

    .xlsx to JSON works.

--- client/src/components/FileUploadForm.js
+++ client/src/components/FileUploadForm.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import xlsx from 'xlsx';
 
 export default function FileUploadForm() {
   const [dataFile, setDataFile] = useState({});
@@ -12,9 +13,25 @@ export default function FileUploadForm() {
     setDataFile(e.target.files);
   };
 
+  const readUploadFile = (e) => {
+    e.preventDefault();
+    if (e.target.files) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const data = e.target.result;
+        const workbook = xlsx.read(data, { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = xlsx.utils.sheet_to_json(worksheet);
+        console.log(json);
+      };
+      reader.readAsArrayBuffer(e.target.files[0]);
+    }
+  };
+
   return (
     <form onSubmit={console.log(dataFile)}>
-      <input type="file" name="picture" onChange={dataFileHandler} />
+      <input type="file" name="picture" onChange={readUploadFile} />
       <button>Submit</button>
     </form>
   );
